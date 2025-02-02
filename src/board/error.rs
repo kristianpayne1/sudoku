a39bc14a78844505ffbaf6a0b18b8597e38770cb
Kristian Payne <kristianpayne1@gmail.com>
2025-02-02 21:01:26 +0000

    Implement handling invalid values

--- src/board/error.rs
+++ src/board/error.rs
@@ -1,6 +1,12 @@
 use std::fmt;
 
 pub struct AlreadyFilledError;
+pub struct InvalidValueError;
+
+pub enum SetValueError {
+    InvalidValue,
+    AlreadyFilledError,
+}
 
 impl fmt::Display for AlreadyFilledError {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
@@ -8,3 +14,6 @@ impl fmt::Display for AlreadyFilledError {
     }
 }
 
+impl fmt::Display for InvalidValueError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result { write!(f, "Value is invalid") }
+}
--- src/game/sudoku.rs
+++ src/game/sudoku.rs
@@ -1,5 +1,5 @@
 use crate::board::cell::Cell;
-use crate::board::error::AlreadyFilledError;
+use crate::board::error::{SetValueError};
 use crate::board::grid::Grid;
 
 pub struct Sudoku {
@@ -11,10 +11,13 @@ impl Sudoku {
         Self { grid: Grid::new() }
     }
 
-    pub fn set_value(&mut self, value: u8, row: usize, col: usize) -> Result<(), AlreadyFilledError> {
+    pub fn set_value(&mut self, value: u8, row: usize, col: usize) -> Result<(), SetValueError> {
+        if (value < 1 || value > 9) {
+            return Err(SetValueError::InvalidValue);
+        }
         match self.grid.get(row, col) {
             Cell::Empty => Ok(self.grid.set(row, col, Cell::Value(value))),
-            Cell::Value(_val) => Err(AlreadyFilledError)
+            Cell::Value(_val) => Err(SetValueError::AlreadyFilledError)
         }
     }
 }

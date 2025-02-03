6a3af32d046cc1bee07cf4c858cb20c7b2fa636a
Kristian Payne <kristianpayne1@gmail.com>
2025-02-03 22:24:18 +0000

    Reformat generator.rs

--- src/helpers/generator.rs
+++ src/helpers/generator.rs
@@ -1,24 +1,29 @@
-use crate::Sudoku;
-use rand::{Rng};
-use std::collections::{HashSet};
 use crate::board::cell::Cell;
+use crate::Sudoku;
+use rand::Rng;
+use std::collections::HashSet;
 
 impl Sudoku {
     fn get_available_values(&self, row: usize, col: usize) -> Vec<u8> {
         let subgrid = self.grid.get_subgrid(row / 3, col / 3);
         let row_list = self.grid.get_row(row);
         let column_list = self.grid.get_col(col);
-        let all_cells = row_list.into_iter().chain(column_list).chain(subgrid.get_all());
+        let all_cells = row_list
+            .into_iter()
+            .chain(column_list)
+            .chain(subgrid.get_all());
         let mut available_values = (1..=9).collect::<HashSet<_>>();
         for cell in all_cells {
             if let Cell::Value(value) = cell {
-                if available_values.contains(value) { available_values.remove(value); }
+                if available_values.contains(value) {
+                    available_values.remove(value);
+                }
             }
         }
 
-       available_values.iter().cloned().collect()
+        available_values.iter().cloned().collect()
     }
-    
+
     pub fn generate(&mut self) {
         for i in 0..self.grid.get_col(0).len() {
             for j in 0..self.grid.get_row(0).len() {
